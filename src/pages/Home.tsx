187d5837ac895ad21732763067e42f2480f933e3
sosler <shamuso23@gmail.com>
2026-08-07 14:43:36 -0500

    Fixed home page view on mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="flex flex-col">
       {!isLoading && images ? (
         <div className="flex flex-col">
-          <section className="grid grid-cols-2 md:grid-cols-3 gap-1 p-1">
+          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 p-1">
             {sortedImages.map((image, i) => (
               <div
                 key={i}
